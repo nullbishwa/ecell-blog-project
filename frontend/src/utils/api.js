9ff098b524efc42d3783eaafb09a472abb2637e4
Bishwaroop Das <195105321+nullbishwa@users.noreply.github.com>
2025-10-18 12:48:13 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance for backend API
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://ecell-blog-project.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://ecell-blog-pn3h.onrender.com/api",
 });
 
 // Attach JWT token to every request if available
